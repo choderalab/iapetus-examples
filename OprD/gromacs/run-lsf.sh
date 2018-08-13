d3c48b47f6c22402a978c39c3c1c66c8bcb2a5fb
John Chodera <john.chodera@choderalab.org>
2018-08-13 15:05:15 -0400

    Add LSF run script

---
+++ OprD/gromacs/run-lsf.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# Set walltime limit
+#BSUB -W 5:59
+#
+# Set output file
+#BSUB -o  iapetus.%J.log
+#
+# Specify node group
+#BSUB -m "ls-gpu lt-gpu lp-gpu lg-gpu"
+#BSUB -q gpuqueue
+#
+# nodes: number of nodes and GPU request
+#BSUB -n 1 -gpu "num=1:j_exclusive=yes:mode=shared" -R "rusage[mem=12]"
+#
+# job name (default = name of script file)
+#BSUB -J "iapetus[0-9]"
+
+jobnames="arg comp7 comp7_nowat comp8 comp8_nowat glu his_neut his_posit imi mero"
+arr=($jobnames)
+jobname=${arr[$LSB_JOBINDEX]}
+echo $jobname
+iapetus --gromacs $jobname --ligseq 423 --output ${jobname}.nc --verbose
